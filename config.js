d22ca662b8fa3e47fcc6ae316f237a26181d26a4
Putu Agus Prana Dhiva Satvika <112155641+odivpds@users.noreply.github.com>
2026-06-25 13:03:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
  * File ini TIDAK AKAN DIACAK, sehingga Client Anda bisa mengedit ini di Github.
  */
 const CONFIG = {
-    // URL API Pusat (Server Utama Anda)
-    API_BASE_URL: 'http://localhost/getfiles-api',
+     // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
+    API_BASE_URL: 'https://api.getfiles.my.id',
 
     // URL Direct Link Pop-under Milik Client (Monetag, dsb)
     CLIENT_POPUNDER_URL: 'https://www.google.com/search?q=iklan-popunder-client'
