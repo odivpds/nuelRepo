3b64a0afbb6c23da6dd41c7fad2461f0ebbc5f13
agusprana <odhivpds01@gmail.com>
2026-06-25 13:45:41 +0800

    Upgrade: Dual mode - Gallery + Player

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * File ini TIDAK AKAN DIACAK, sehingga Client Anda bisa mengedit ini di Github.
  */
 const CONFIG = {
-     // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
+    // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
     API_BASE_URL: 'https://api.getfiles.my.id',
 
     // URL Direct Link Pop-under Milik Client (Monetag, dsb)
