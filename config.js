3d18a23d654d38d6ee882fdeca6a7e84b2eaaf7b
agusprana <odhivpds01@gmail.com>
2026-06-25 12:56:20 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * KONFIGURASI CLIENT AFFILIATE
+ * File ini TIDAK AKAN DIACAK, sehingga Client Anda bisa mengedit ini di Github.
+ */
+const CONFIG = {
+    // URL API Pusat (Server Utama Anda)
+    API_BASE_URL: 'http://localhost/getfiles-api',
+
+    // URL Direct Link Pop-under Milik Client (Monetag, dsb)
+    CLIENT_POPUNDER_URL: 'https://www.google.com/search?q=iklan-popunder-client'
+};
