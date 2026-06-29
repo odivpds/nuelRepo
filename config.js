a1bc6ebc47fadc157cf986ad3b929c3eaf34b990
agusprana <odhivpds01@gmail.com>
2026-06-30 01:57:54 +0800

    set-up nuel

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ const CONFIG = {
     // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
     API_BASE_URL: 'https://api.getfiles.my.id',
 
-    // URL Direct Link Pop-under Milik Client (Monetag, dsb)
-    CLIENT_POPUNDER_URL: 'https://www.google.com/search?q=iklan-popunder-client'
+    // URL Direct Link Pop-under Milik Client (Kosongkan jika menggunakan script di index.html)
+    CLIENT_POPUNDER_URL: ''
 };
